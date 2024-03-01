974a451e1003f630e247135813c2d2e66842df07
Natalia Bukovetskaya <fariganatalia@gmail.com>
2024-03-01 12:18:30 +0000

    :q!

--- src/js/main.js
+++ src/js/main.js
@@ -17,3 +17,20 @@ dropdown[i].addEventListener('click', function() {
   dropdown[i].querySelector('.dropdown__menu').classList.toggle('dropdown__menu--open');
 });
 }
+
+const rows = document.querySelectorAll('.student-data__body .student-data__checkbox');
+rows.forEach(row => {
+  let line = row.parentNode;
+  row.addEventListener('click', () => {
+    if (row.querySelector('input').checked) {
+      line.classList.add('student-data__row--select');
+    } else 
+    line.classList.remove('student-data__row--select');
+  });
+});
+
+const headName = document.querySelector('.student-data__head .student-data__checkbox').addEventListener('click', () => {
+  rows.forEach(row => {
+    row.querySelector('input').checked = document.querySelector('.student-data__head .student-data__checkbox input').checked; 
+  })
+});
